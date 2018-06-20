52959bd9bdcc61959d6e8a6620806385ab026844
nexmdev <dev.nexm@gmail.com>
2018-06-20 18:47:19 +0530

    20-6-2018

--- app/src/main/java/ltd/akhbod/omclasses/ViewHolders/SearchByDate_Search.java
+++ app/src/main/java/ltd/akhbod/omclasses/ViewHolders/SearchByDate_Search.java
@@ -18,29 +18,53 @@ public class SearchByDate_Search extends RecyclerView.ViewHolder {
 
     TextView mName;
     View mView;
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int position);
+    }
+    private static SearchByDate_Search.OnItemClickListener listener;
 
-    public SearchByDate_Search(View itemView) {
+    public void setOnItemClickListener(OnItemClickListener listener){
+        SearchByDate_Search.listener = listener;
+    }
+
+
+    public SearchByDate_Search(final View itemView) {
         super(itemView);
         mView = itemView;
         mName=mView.findViewById(R.id.search_singleLayout_name);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int position = getAdapterPosition();
+
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(itemView,position);
+                    }
+                }
+            }
+        });
     }
 
-    public void setDetails(final SearchByDateDetails model, final Context applicationContext, final String key, String searchText, final String mSelectedStanderdText) {
+    public void setDetails(final SearchByDateDetails model, final Context applicationContext,String key,  String searchText, final String mSelectedStanderdText) {
 
 
         String testText=key.replace(searchText+"-","");
         mName.setText(testText);
-        mName.setOnClickListener(new View.OnClickListener() {
+       /* mName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 Intent intent = new Intent(applicationContext, RecordTestActivity.class);
                 intent.putExtra("key",key);
                 intent.putExtra("totalPresent",model.getTotalPresent());
                 intent.putExtra("class",mSelectedStanderdText);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 applicationContext.startActivity(intent);
 
             }
-        });
+        });*/
 
     }
 }
