58a03ad1be5d42300a5e8ebbb16caa92d6ea3a4b
git1998 <abhibodulwar007@gmail.com>
2018-07-01 19:21:05 +0530

    code for smsBtn  @RecordTest adapter

--- app/src/main/java/ltd/akhbod/omclasses/Adapter/RecordTest.java
+++ app/src/main/java/ltd/akhbod/omclasses/Adapter/RecordTest.java
@@ -1,6 +1,7 @@
 package ltd.akhbod.omclasses.Adapter;
 
 import android.content.Context;
+import android.telephony.SmsManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -9,7 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -27,19 +33,34 @@ public class RecordTest extends BaseAdapter {
     int TotalStudentCount=0;
 
     public ArrayList<String> marksArray=new ArrayList<>();
-    public String[] noToUpload=null;
+    public String[] noToUpload;
     ArrayList<String> studentIdArray=new ArrayList<>();
     ArrayList<String> studentNamesArray=new ArrayList<>();
+    ArrayList<Boolean> smsSendArray=new ArrayList<>();
+    ArrayList<String> mobNoArray=new ArrayList<>();
+
+    String dateOfTest,subjectOfTest,mSelectedStanderd,durationText;
 
 
-    public RecordTest(Context c,ArrayList<String> marksArray ,ArrayList<String> studentIds, ArrayList<String> studentNames, String key, int totalCount){
+    public RecordTest(Context c, ArrayList<String> marksArray, ArrayList<String> studentIds, ArrayList<String> studentNames,
+                      String key, int totalCount, ArrayList<Boolean> smsSendArray, ArrayList<String> mobNoArray, String mSelectedStanderd, String durationText){
         this.c=c;
+        this.key=key;
+        this.noToUpload=new String[totalCount];
+        this.TotalStudentCount=totalCount;
+        this.mSelectedStanderd= mSelectedStanderd;
+        this.durationText= durationText;
+
         this.marksArray=marksArray;
         this.studentIdArray=studentIds;
         this.studentNamesArray=studentNames;
-        this.key=key;
-        this.TotalStudentCount=totalCount;
-        this.noToUpload=new String[totalCount];
+        this.smsSendArray=smsSendArray;
+        this.mobNoArray=mobNoArray;
+
+
+        String[] parts=key.split("-");
+        dateOfTest=parts[0]+"/"+parts[1]+"/"+parts[2]+"/";
+        subjectOfTest=parts[3];
     }
 
     @Override
@@ -65,14 +86,33 @@ public class RecordTest extends BaseAdapter {
         View row=inflater.inflate(R.layout.recordtest_singlelayout,parent,false);
 
 
-        TextView mPosition=row.findViewById(R.id.recordtest_single_postion);
-        TextView mName=row.findViewById(R.id.recordtest_single_name);
+        final TextView mPosition=row.findViewById(R.id.recordtest_single_postion);
+        final  TextView mName=row.findViewById(R.id.recordtest_single_name);
+        final ImageButton mSend=row.findViewById(R.id.recordtest_single_massageBtn);
+        final ProgressBar mPreogressBar=row.findViewById(R.id.recordtest_single_row_progressbar);
         final EditText mEditMarks=row.findViewById(R.id.recordtest_single_editMarksEdit);
 
 
-        if(!marksArray.get(position).isEmpty())
+        if(smsSendArray.get(position))
         {
+            mSend.setBackgroundResource(R.drawable.ic_message_black_24dp_grey);                      //setting sms view
+            mSend.setEnabled(false);
+            mSend.setImageResource(R.drawable.ic_done_black_24dp);
+        }
 
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSend.setBackgroundResource(R.drawable.ic_message_black_24dp_grey);
+                mSend.setEnabled(false);
+                mSend.setImageResource(R.drawable.ic_done_black_24dp);
+                sendMessage(mobNoArray.get(position),studentNamesArray.get(position),position);
+
+            }
+        });
+
+        if(!marksArray.get(position).isEmpty())                                                      //setting marks view
+        {
             mEditMarks.setText(marksArray.get(position));
             mEditMarks.setEnabled(false);
             noToUpload[position]="no";
@@ -102,6 +142,19 @@ public class RecordTest extends BaseAdapter {
         mName.setText(studentNamesArray.get(position));
 
         return row;
+    }
+
+    private void sendMessage(String mobNo, String name, int position) {
+
+        SmsManager manager = SmsManager.getDefault();
+        String test = "आपला पाल्य "+name+" दि."+dateOfTest+" ला झालेल्या " +subjectOfTest +" च्या टेस्ट ला अनुपस्थित होता. ओम क्लासेस";
+        ArrayList<String> parts = manager.divideMessage(test);
+        manager.sendMultipartTextMessage("+918329575298",null,parts,null,null);
+
+        FirebaseDatabase.getInstance().getReference().child(mSelectedStanderd+durationText).child("record")
+                .child(studentIdArray.get(position) + "/" + key + "/" + "isSmsSent")
+                .setValue(true);
+
     }
 
     public ArrayList<String> getMarks(){
